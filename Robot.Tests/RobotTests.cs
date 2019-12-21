f443933a5e794058f95fee7f5526c8e659f27a03
Gary Hughes <garyedwardhughes@gmail.com>
2019-12-21 16:42:17 +1100

    Implement left and right.

--- Robot.Tests/RobotTests.cs
+++ Robot.Tests/RobotTests.cs
@@ -93,7 +93,88 @@ namespace Robot.Tests
             Assert.IsNull(robot.Report());
         }
 
+        [TestMethod]
+        public void TestLeftBeforeValidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsFalse(robot.Left());
+        }
 
+        [TestMethod]
+        public void TestRightBeforeValidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsFalse(robot.Right());
+        }
 
+        [TestMethod]
+        public void TestLeftAfterInvalidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsFalse(robot.Place(new Position(new Coordinate(10, 10), Direction.North)));
+            Assert.IsFalse(robot.Left());
+        }
+
+        [TestMethod]
+        public void TestRightAfterInvalidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsFalse(robot.Place(new Position(new Coordinate(10, 10), Direction.North)));
+            Assert.IsFalse(robot.Right());
+        }
+
+        [TestMethod]
+        public void TestLeftAfterValidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsTrue(robot.Place(new Position(new Coordinate(1, 2), Direction.North)));
+            Assert.IsTrue(robot.Left());
+        }
+
+        [TestMethod]
+        public void TestRightAfterValidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsTrue(robot.Place(new Position(new Coordinate(1, 2), Direction.North)));
+            Assert.IsTrue(robot.Right());
+        }
+
+        [TestMethod]
+        public void TestFullTurnLeftAfterValidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsTrue(robot.Place(new Position(new Coordinate(1, 2), Direction.North)));
+            Assert.IsTrue(robot.Left());
+            Assert.AreEqual(Direction.West, robot.Report()?.Direction);
+            Assert.IsTrue(robot.Left());
+            Assert.AreEqual(Direction.South, robot.Report()?.Direction);
+            Assert.IsTrue(robot.Left());
+            Assert.AreEqual(Direction.East, robot.Report()?.Direction);
+            Assert.IsTrue(robot.Left());
+            Assert.AreEqual(Direction.North, robot.Report()?.Direction);
+        }
+
+        [TestMethod]
+        public void TestFullTurnRightAfterValidPlacement()
+        {
+            var world = new World(width:5, height:5);
+            var robot = new Robot(world);
+            Assert.IsTrue(robot.Place(new Position(new Coordinate(1, 2), Direction.North)));
+            Assert.IsTrue(robot.Right());
+            Assert.AreEqual(Direction.East, robot.Report()?.Direction);
+            Assert.IsTrue(robot.Right());
+            Assert.AreEqual(Direction.South, robot.Report()?.Direction);
+            Assert.IsTrue(robot.Right());
+            Assert.AreEqual(Direction.West, robot.Report()?.Direction);
+            Assert.IsTrue(robot.Right());
+            Assert.AreEqual(Direction.North, robot.Report()?.Direction);
+        }
     }
 }
--- Robot/Robot.cs
+++ Robot/Robot.cs
@@ -26,11 +26,35 @@ namespace Robot
 
         public bool Left()
         {
+            if (_position is Position position) {
+                var newDirection = position.Direction switch {
+                    Direction.North => Direction.West,
+                    Direction.East => Direction.North,
+                    Direction.South => Direction.East,
+                    Direction.West => Direction.South,
+                    _ => throw new Exception($"Invalid value for direction {position.Direction}")
+                };
+                _position = new Position(position.Coordinate, newDirection);
+                return true;
+            }
+
             return false;
         }
 
         public bool Right()
         {
+            if (_position is Position position) {
+                var newDirection = position.Direction switch {
+                    Direction.North => Direction.East,
+                    Direction.East => Direction.South,
+                    Direction.South => Direction.West,
+                    Direction.West => Direction.North,
+                    _ => throw new Exception($"Invalid value for direction {position.Direction}")
+                };
+                _position = new Position(position.Coordinate, newDirection);
+                return true;
+            }
+
             return false;
         }
 
