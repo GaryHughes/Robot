bd81d8e997dfbff8949a1497295dac0e7e008b54
Gary Hughes <garyedwardhughes@gmail.com>
2019-12-27 15:46:57 +1100

    Basic layout and API interface.

--- Robot.Frontend/src/components/Home.js
+++ Robot.Frontend/src/components/Home.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight, faChevronUp, faChevronDown, faUndo, faRedo, faWalking } from '@fortawesome/free-solid-svg-icons'
+import * as api from './RobotApi'
+import './Home.css'
 
 export class Home extends Component {
-  static displayName = Home.name;
 
   render () {
+    const rows = 5;
+    const cols = 5;
     return (
-      <div>
-        <h1>Robot</h1>
+      <div className="grid">
+      <br></br>
+        {
+          Array.from({ length: rows }, (value, row) => { 
+            let rowKey = `${row}`;
+            return (
+              <div className="row" key={rowKey}>
+              {
+                Array.from({ length: cols }, (value, col) => {
+                  let key = `${row}=${col}`;
+                  return (
+                    <div className="box" key={key}>
+                      <div className="inner" onClick={e => Robot.place(row, col)}>
+                        
+                      </div>
+                    </div>
+                  );
+                })
+              }
+              </div>
+            )
+          }).reverse()
+        }
+        <br></br>
+        <div className="row">
+          <div className="box"><div className="inner" title="Face West" onClick={api.faceWest}><FontAwesomeIcon icon={faChevronLeft} /></div></div>
+          <div className="box"><div className="inner" title="Face North" onClick={api.faceNorth}><FontAwesomeIcon icon={faChevronUp} /></div></div>
+          <div className="box"><div className="inner" title="Face East" onClick={api.faceEast}><FontAwesomeIcon icon={faChevronRight} /></div></div>
+          <div className="box"><div className="inner" title="Face South" onClick={api.faceSouth}><FontAwesomeIcon icon={faChevronDown} /></div></div>
+          <div className="box"><div className="inner" title="Turn Left" onClick={api.turnLeft}><FontAwesomeIcon icon={faUndo} /></div></div>
+          <div className="box"><div className="inner" title="Turn Right" onClick={api.turnRight}><FontAwesomeIcon icon={faRedo} /></div></div>
+          <div className="box"><div className="inner" title="Move Forward" onClick={api.move}><FontAwesomeIcon icon={faWalking} /></div></div>
+        </div>
       </div>
     );
   }
---
+++ Robot.Frontend/src/components/RobotApi.js
@@ -0,0 +1,33 @@
+
+
+export function faceWest(ev) {
+    console.log("Face West");
+}
+
+export function faceNorth() {
+    console.log("Face North");
+}
+
+export function faceEast() {
+    console.log("Face East");
+}
+
+export function faceSouth() {
+    console.log("Face South");
+}
+
+export function turnLeft() {
+    console.log("Turn Left");
+}
+
+export function turnRight() {
+    console.log("Turn Right");
+}
+
+export function move() {
+    console.log("Move");
+}
+
+export function place(x, y) {
+   console.log(`Place ${x} ${y}`) 
+}
