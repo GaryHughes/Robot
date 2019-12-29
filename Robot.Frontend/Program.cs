fcd3e9bf6c66d59c1452b824ea8c7d263cf8c20c
Gary Hughes <garyedwardhughes@gmail.com>
2019-12-29 12:58:47 +1100

    URL confguration.

--- Robot.Frontend/Program.cs
+++ Robot.Frontend/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +20,16 @@ namespace Robot.Frontend
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) => {
+                    if (!Uri.TryCreate(Assembly.GetExecutingAssembly().CodeBase, UriKind.RelativeOrAbsolute, out var uri)) {
+                        throw new Exception("Cannot determine file system path of executing assembly");
+                    }
+                    var path = Path.GetDirectoryName(uri.LocalPath);
+                    if (string.IsNullOrEmpty(path)) {
+                        throw new Exception("Cannot determine file system path of executing assembly");
+                    }
+                    config.AddJsonFile(Path.Combine(path, "hosting.json"), optional:true, reloadOnChange:true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
